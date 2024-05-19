530c1c72a7d7bac85455218fa432754e4462020d
Vellaisamy-24 <vellaikarthick24@gmail.com>
2024-05-19 08:48:23 +0530

    tailwind css added

--- realestatemern/src/App.js
+++ realestatemern/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div className="text-red-400 font-bold my-10">App</div>;
+};
 
-export default App
+export default App;
