ecdcdf5b661abd077fb7fc31498f12254feb1078
Vellaisamy-24 <vellaikarthick24@gmail.com>
2024-06-13 18:13:46 +0530

    user lisint by user id api integrated

--- realestatemern/src/CategoryListing/UserCategoryListing.jsx
+++ realestatemern/src/CategoryListing/UserCategoryListing.jsx
@@ -1,19 +1,43 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const UserCategoryListing = () => {
   const user = useSelector((state) => state.user.currentUser);
-  const fetchUserCategoryListing =async () => {
+  useEffect(() => {
+    fetchUserCategoryListing();
+  }, [user]);
+  const [userCategoryListing, setUserCategoryListing] = useState([]);
+  const fetchUserCategoryListing = async () => {
     try {
-        const response=await axios.get()
+      const response = await axios.get(
+        `http://localhost:5000/api/data/getUserCategoryListing/${user._id}`
+      );
+      console.log(response);
+      setUserCategoryListing(response.data?.userCategoryListing);
     } catch (error) {
       console.log(error);
     }
   };
   return (
     <div>
-      UserCategoryListing:{user.email}:{user._id}
+      {/* UserCategoryListing:{user.email}:{user._id} */}
+      <div>
+        <h1>UserCategoryListing</h1>
+        {userCategoryListing &&
+          userCategoryListing.map((data, index) => (
+            <div className="py-5 px-2">
+              {data.name}
+              {data.description}
+              {data.address}
+              {data.images && data.images.map((img)=>
+            (
+                <>
+                <img src={img} className="w-10 h-10"/></>
+            ))}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
