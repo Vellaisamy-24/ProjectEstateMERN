97de6e34f6206cacd7feab20320936a9113bfbd7
Vellaisamy-24 <vellaikarthick24@gmail.com>
2024-06-13 08:23:16 +0530

    CategoryListing model created

---
+++ backend/models/categoryListing.jsx
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const schema = new mongoose.Schema({
+  userRef: {
+    id: String,
+    email: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  regularPrice: {
+    type: Number,
+  },
+  discountPrice: {
+    type: Number,
+  },
+  bathRooms: {
+    type: Number,
+  },
+  bedRooms: {
+    type: Number,
+  },
+  furnished: {
+    type: Boolean,
+  },
+  offer: {
+    type: Boolean,
+  },
+  rent: {
+    type: Boolean,
+  },
+  parking: {
+    type: Boolean,
+  },
+  sell: {
+    type: Boolean,
+  },
+  images: {
+    type: Array,
+  },
+});
+const CategoryListing=mongoose.model('categoryListing',schema)
+module.exports=CategoryListing
