1f41db017b5513bc28f75bc0b32e0c34a2614a21
Vellaisamy-24 <vellaikarthick24@gmail.com>
2024-06-13 23:32:06 +0530

    sample ui for user getCategoryListing

--- realestatemern/src/CategoryListing/UserCategoryListing.jsx
+++ realestatemern/src/CategoryListing/UserCategoryListing.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const UserCategoryListing = () => {
   const user = useSelector((state) => state.user.currentUser);
@@ -26,15 +27,76 @@ const UserCategoryListing = () => {
         <h1>UserCategoryListing</h1>
         {userCategoryListing &&
           userCategoryListing.map((data, index) => (
-            <div className="py-5 px-2">
-              {data.name}
-              {data.description}
-              {data.address}
-              {data.images && data.images.map((img)=>
-            (
-                <>
-                <img src={img} className="w-10 h-10"/></>
-            ))}
+            <div key={index} className="py-5 px-2 border">
+              <Link
+                to={`/updateCategoryListing/${data._id}`}
+                className=" flex right-0 fixed px-10"
+              >
+                Update
+              </Link>
+              <div>
+                <div className="flex gap-4  flex-wrap">
+                  {data.images &&
+                    data.images.map((img) => (
+                      <div className="p-10">
+                        <img
+                          src={img}
+                          className="w-30 h-10 rounded-sm shadow-lg"
+                        />
+                      </div>
+                    ))}
+                </div>
+
+                <div className="px-10 flex flex-col gap-2 ">
+                  <h1> Name: {data.name}</h1>
+                  <p>Description: {data.description}</p>
+                  <span>Address:{data.address}</span>
+                  <div className="flex gap-8 flex-wrap">
+                    <span>BedRooms:{data.bedRooms}</span>
+                    <span>BathRooms:{data.bathRooms}</span>
+                  </div>
+                  <div className="flex flex-wrap gap-4">
+                    <span>
+                      <input type="checkbox" checked={data.offer} />
+                      Offer
+                    </span>
+                    <span>
+                      <input type="checkbox" checked={data.parking} />
+                      Parking
+                    </span>
+                    <span>
+                      <input type="checkbox" checked={data.furnished} />
+                      Furnished
+                    </span>
+                    <span>
+                      <input type="checkbox" checked={data.rent} />
+                      Rent
+                    </span>
+                    <span>
+                      <input type="checkbox" checked={data.sell} />
+                      Sell
+                    </span>
+                  </div>
+                  <div className="flex gap-4 flex-wrap">
+                    <span>
+                      RegularPrice:
+                      <input
+                        type="number"
+                        className="border"
+                        value={data.regularPrice}
+                      />
+                    </span>
+                    <span>
+                      DiscountPrice:
+                      <input
+                        type="number"
+                        className="border"
+                        value={data.discountPrice}
+                      />
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
       </div>
